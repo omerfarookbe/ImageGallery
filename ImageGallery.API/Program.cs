c4566a00b665837985bdf134a68ea7c521badc43
Omer Farook <49291618+omerfarookbe@users.noreply.github.com>
2024-07-17 17:14:09 -0400

    api project

---
+++ ImageGallery.API/Program.cs
@@ -0,0 +1,66 @@
+using ImageGallery.API.DbContexts;
+using ImageGallery.API.Services;
+using ImageGallery.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.JsonWebTokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers()
+    .AddJsonOptions(configure => configure.JsonSerializerOptions.PropertyNamingPolicy = null);
+
+builder.Services.AddDbContext<GalleryContext>(options =>
+{
+    options.UseSqlite(
+        builder.Configuration["ConnectionStrings:ImageGalleryDBConnectionString"]);
+});
+
+// register the repository
+builder.Services.AddScoped<IGalleryRepository, GalleryRepository>();
+
+// register AutoMapper-related services
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+JsonWebTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = "https://localhost:5001";
+        options.Audience = "imagegalleryapi";
+        options.TokenValidationParameters.ValidTypes = new[] { "at+jwt" };
+        options.TokenValidationParameters = new()
+        {
+            NameClaimType = "given_name",
+            RoleClaimType = "role",
+            ValidTypes = new[] { "at+jwt" }
+        };
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("UserCanAddImage", AuthorizationPolicies.CadAddImage());
+
+    options.AddPolicy("ClientApplicationCanWrite", policybuilder =>
+    {
+        policybuilder.RequireClaim("scope", "imagegalleryapi.write");
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
